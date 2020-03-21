d075dd79a8ceed717468166b3631f9ab01cd7a43
Petros Kitsaras <petros.kitsaras@gmail.com>
2020-03-21 21:10:43 +0100

    circleci setup"

--- src/main/java/thegamerurso/springframework/springrecipeapp/controllers/IndexController.java
+++ src/main/java/thegamerurso/springframework/springrecipeapp/controllers/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import thegamerurso.springframework.springrecipeapp.services.RecipeService;
+
 @Slf4j
 @Controller
 public class IndexController
